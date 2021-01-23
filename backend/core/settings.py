c337c3221dd778b5adab4a39a99bebd486fcaeff
ameedasmah <ameed.asmah@hotmail.com>
2021-01-23 23:49:24 +0200

    user done

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'djoser',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+AUTH_USER_MODEL = 'users.UserAccount'
