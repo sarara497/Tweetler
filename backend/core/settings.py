7c4f37bd8596c393a37feb50b620798f9d8097df
ameedasmah <ameed.asmah@hotmail.com>
2021-01-24 00:46:00 +0200

    done with follwers

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'djoser',
     'users',
     'tweets',
+    'followers',
 ]
 
 MIDDLEWARE = [
---
+++ backend/followers/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from users.models import UserAccount
+
+# Create your models here.
+
+
+class Follow(models.Model):
+    following_From = models.ForeignKey(
+        UserAccount, on_delete=models.CASCADE, related_name='following_From')
+    following_To = models.ForeignKey(
+        UserAccount, on_delete=models.CASCADE, related_name='following_To')
+    time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return "%s %s" % (self.following_From.name, self.following_To.name)
