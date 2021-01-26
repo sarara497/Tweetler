bdcdf0d3f851d21175f72dfc3bd72291f0448df4
ameedasmah <67121588+ameedasmah@users.noreply.github.com>
2021-01-26 19:33:41 +0200

    auth

--- backend/likes/serializers.py
+++ backend/likes/serializers.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-from rest_framework import serializers
-from likes.models import Like
-
-
-class LikeSerializer(serializers.ModelSerializer):
-    user = serializers.CharField(source='user_Id.name', read_only=True)
-
-    class Meta:
-        model = Like
-        fields = ('id', 'user', 'user_Id', 'tweet_Id')
-=======
 from rest_framework import serializers
 from likes.models import Like
 
@@ -19,5 +7,4 @@ class LikeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Like
-        fields = ('id', 'tweet')
->>>>>>> cedbc2c8e1818523c80834e81c6c5fcec48a4ab6
+        fields = ('id', 'user')
