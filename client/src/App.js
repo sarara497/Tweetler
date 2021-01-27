4a33a14ac339b32af4eb589eddc965b39627da31
Yasir Tobbileh <yasur96@gmail.com>
2021-01-27 16:26:41 +0200

    feat/ create navbar

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,16 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import LandingPage from "./Pages/LandingPage/LandingPage";
 import LoginPage from "./Pages/LoginPage/LoginPage";
+import SideNavbar from "./Components/SideNavbar/SideNavbar";
 // import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-
         <Switch>
           <Route exact path="/" component={LandingPage} />
+          <Route path="/home" component={SideNavbar} />
           <Route path="/login" component={LoginPage} />
         </Switch>
 
