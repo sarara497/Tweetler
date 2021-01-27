8c9584efbbd73a2a81fa10bfd24c551e7ab7e0a2
ameedasmah <ameed.asmah@hotmail.com>
2021-01-27 12:49:56 +0200

    someBugs

--- backend/comments/views.py
+++ backend/comments/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from rest_framework import generics
+
+from comments.models import Comment
+from comments.serializers import CommentSerializer
 # Create your views here.
+
+
+class CommentList(generics.ListCreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
--- backend/likes/models.py
+++ backend/likes/models.py
@@ -6,8 +6,14 @@ from tweets.models import Tweet
 
 
 class Like(models.Model):
-    user_Id = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
-    tweet_Id = models.ForeignKey(Tweet, on_delete=models.CASCADE)
+    user_Id = models.ForeignKey(
+        UserAccount, on_delete=models.CASCADE, related_name='user_Likes')
+    tweet_Id = models.ForeignKey(
+        Tweet, on_delete=models.CASCADE, related_name='tweet_Likes')
+
+
+class Meta:
+        unique_together = ('user_Id', 'tweet_Id')
 
     def __str__(self):
         return self.user_Id.name
