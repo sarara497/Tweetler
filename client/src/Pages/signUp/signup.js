c582dd42075085e99aab829eb94d32fc56547015
sarara497 <sara19972020@outlook.com>
2021-01-30 01:04:44 +0200

    edit signUp  Page

--- client/src/Pages/signUp/signup.js
+++ client/src/Pages/signUp/signup.js
@@ -4,8 +4,6 @@ import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
@@ -13,6 +11,9 @@ import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import { GiHummingbird } from "react-icons/gi";
+
+import './signup.css'
 
 function Copyright() {
     return (
@@ -20,9 +21,9 @@ function Copyright() {
             {'Copyright © '}
             <Link color="inherit" href="https://material-ui.com/">
                 Your Website
-      </Link>{' '}
+      </Link>
             {new Date().getFullYear()}
-            {'.'}
+           
         </Typography>
     );
 }
@@ -30,23 +31,31 @@ function Copyright() {
 const useStyles = makeStyles((theme) => ({
 
     paper: {
-        marginTop: theme.spacing(8),
+        marginTop: theme.spacing(5),
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
     },
     avatar: {
+        width:'30%',
+        height:'100px',
         margin: theme.spacing(1),
-        backgroundColor: theme.palette.secondary.main,
+        backgroundColor: theme.palette.info.main,
     },
     form: {
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing(3),
+       
     },
     submit: {
         margin: theme.spacing(3, 0, 2),
+        backgroundColor: theme.palette.info.main,
     },
-}));
+    textcolor:{
+         border:'#2196f3',
+         borderRadius:'4px',
+        }
+        }));
 
 export default function SignUp({ signupFun }) {
     const classes = useStyles();
@@ -89,11 +98,11 @@ export default function SignUp({ signupFun }) {
     }
 
     return (
-        <Container component="main" maxWidth="xs">
+        <Container component="main" maxWidth="xs" style={{border:'groove' , borderColor:'#2196f38f' , borderRadius:'12px' , marginTop:'40px'  ,marginBottom:'40px'}}>
             <CssBaseline />
             <div className={classes.paper}>
                 <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
+                    <GiHummingbird style={{fontSize:'85px'}}/>
                 </Avatar>
                 <Typography component="h1" variant="h5">
                     Sign up
@@ -102,6 +111,7 @@ export default function SignUp({ signupFun }) {
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={12}>
                             <TextField
+                            className={classes.textcolor}
                                 autoComplete="fname"
                                 name="Name"
                                 value={name}
@@ -118,6 +128,7 @@ export default function SignUp({ signupFun }) {
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
+                            className={classes.textcolor}
                                 variant="outlined"
                                 required
                                 fullWidth
@@ -133,6 +144,7 @@ export default function SignUp({ signupFun }) {
                         </Grid>
                         <Grid item xs={12}>
                             <TextField
+                            className={classes.textcolor}
                                 variant="outlined"
                                 required
                                 fullWidth
@@ -158,18 +170,15 @@ export default function SignUp({ signupFun }) {
                     >
                         Sign Up
           </Button>
-                    <Grid container justify="flex-end">
-                        <Grid item>
-                            <Link href="#" variant="body2">
+                    <Grid container justify="flex-end" >
+                        <Grid item >
+                            <Link href="/login" variant="body2" >
                                 Already have an account? Sign in
               </Link>
                         </Grid>
                     </Grid>
                 </form>
             </div>
-            <Box mt={5}>
-                <Copyright />
-            </Box>
         </Container>
     );
 }
